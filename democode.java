cc4a84c07385e5c53ce6ac14af0563633e3fd173
Aditya Yadav <ay.aditya22@.com>
2026-04-20 10:07:56 +0530

    second commit

---
+++ democode.java
@@ -0,0 +1,20 @@
+public class HelloDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Simple variables
+        int a = 5;
+        int b = 3;
+
+        // Basic operation
+        int sum = a + b;
+
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+        // Loop example
+        System.out.println("Counting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
